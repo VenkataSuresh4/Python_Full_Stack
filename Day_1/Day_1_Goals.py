467d7d1f7bada5f6a2b83decc8e7ac72651212a4
Suresh Reddy <venkatareddy0411@gmail.com>
2023-12-26 00:41:00 +0530

    Day_1 Completed Code Including Practise

--- Day_1/Day_1_Goals.py
+++ Day_1/Day_1_Goals.py
@@ -1,1 +1,9 @@
 print("Welcome to the Band Name Generator.")
+
+# Fetch the CIty Name
+city_name = input("What's name of the city you grew up in?\n")
+
+# Fetch the Pet Name
+pet_name = input("What's your pet's name?\n")
+
+print("Your band name could be " + city_name + " " + pet_name)
--- Day_1/Day_1_Practise.py
+++ Day_1/Day_1_Practise.py
@@ -1,2 +1,42 @@
 # This file is for Practising the code before creating the project
+
+# First Program - print to console
 print("Hello World")
+
+# Write a program to print the below statement
+# Day 1 - Python Print Function
+# The function is declared like this;
+# print('what to print')
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+# Print Hello World in 3 lines using one line code
+print("Hello World!\nHello World!\nHello World!")
+
+# Fix the below code
+# print(Day 1 - String Manipulation")
+# print("String Concatenation is done with the "+" sign.")
+#   print('e.g. print("Hello " + "world")')
+# print(("New lines can be created with a backslash and n.")
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+# Print the number of characters of the given string
+
+print(len(input("\n")))
+
+# Swap a and b values
+a = input()
+b = input()
+
+c = a
+a = b
+b = c
+
+print(a)
+print(b)
