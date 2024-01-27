dd1352249579d2a9645cb03d037ef14eb36cc502
Suresh Reddy <venkatareddy0411@gmail.com>
2024-01-27 22:58:45 +0530

    Day_8 Code Completed with Practice

---
+++ Day_8/Day_8_Goals.py
@@ -0,0 +1,51 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
+
+want_continue = False
+
+
+def encrypt(text, shift):
+    new_word = []
+    for letter in text:
+        current_index = alphabet.index(letter)
+        new_word.append(alphabet[current_index + shift])
+
+    output_text = ""
+    for word in new_word:
+        output_text += word
+
+    print(output_text)
+
+
+def decrypt(text, shift):
+    new_word = []
+    for letter in text:
+        current_index = alphabet.index(letter)
+        new_word.append(alphabet[current_index - shift])
+
+    output_text = ""
+    for word in new_word:
+        output_text += word
+
+    print(output_text)
+
+
+while not want_continue:
+
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    if shift > 26:
+        
+
+    if direction == "encode":
+        encrypt(text, shift)
+    elif direction == "decode":
+        decrypt(text, shift)
+
+    do_it_again = input("Do you want to continue [Yes or No] ")
+    if do_it_again == 'No':
+        want_continue = True
